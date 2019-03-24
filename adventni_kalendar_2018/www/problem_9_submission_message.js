5dbf495d2d216b885358b262242a6d0882863a19
Lubomír Štěpánek <lubomir.stepanek@gmail.com>
2019-03-24 09:29:40 +0100

    Add files via upload

---
+++ adventni_kalendar_2018/www/problem_9_submission_message.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $('#problem_9_submitting').click(function() {
+        alert(
+            'Pokud jsi zadal správné heslo, tvoje řešení bylo uloženo, checkni svoje aktuální body v záložce "Řešitelé". Špatně zadané heslo nezmění danému řešiteli stav bodů ani pokusů.'
+        );
+		/*setInterval();*/
+    });
+
+});
